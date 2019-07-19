0c71b11dfa18666cb0379e51b84a1845a9303fb0
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-18 22:16:06 -0700

    cleaned up comments

--- mean-angular/src/app/posts/post-list/post-list.component.ts
+++ mean-angular/src/app/posts/post-list/post-list.component.ts
@@ -8,11 +8,5 @@ import { Post } from '../post.model';
 })
 
 export class PostListComponent {
-  // posts = [
-  //   { title: 'The first post', content: 'Things and stuff' },
-  //   { title: 'The second post', content: 'Stuff and things' },
-  //   { title: 'The third post', content: 'Other things and more stuff' },
-  //   { title: 'The fourth post', content: 'More things and other stuff' }
-  // ];
   @Input() posts: Post[] = [];
 }
