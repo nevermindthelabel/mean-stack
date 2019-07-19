477cfd6dfc1d91a366f84333512e45cfc8ede872
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-18 22:13:17 -0700

    imported post interface

--- mean-angular/src/app/app.component.ts
+++ mean-angular/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-angular';
+  allPosts: Post[] = [];
+
+  onNewPost(post) {
+    this.allPosts.push(post);
+  }
 }
