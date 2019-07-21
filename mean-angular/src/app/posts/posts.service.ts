d2bfff3c01686d1d0d765f50da3ab3a65d2dadc0
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-20 21:09:15 -0700

    removed events and added subscriptions

--- mean-angular/src/app/posts/posts.service.ts
+++ mean-angular/src/app/posts/posts.service.ts
@@ -1,12 +1,18 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     return [...this.posts];
+  }
+
+  getPostUpdatedObserver() {
+    return this.postsUpdated.asObservable();
   }
 
   addPost(title: string, content: string) {
@@ -15,5 +21,6 @@ export class PostsService {
       content
     };
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
