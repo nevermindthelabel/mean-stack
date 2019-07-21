b244ac9840da5fa6483b1977ada2a38f6f20db0b
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-20 21:07:01 -0700

    removed event emitters

--- mean-angular/src/app/app.component.ts
+++ mean-angular/src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
 
-import { Post } from './posts/post.model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  allPosts: Post[] = [];
-
-  onNewPost(post) {
-    this.allPosts.push(post);
-  }
-}
+export class AppComponent {}
